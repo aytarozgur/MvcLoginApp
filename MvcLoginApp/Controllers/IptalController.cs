f8b5a642727962e08748754a41d97a52daff53db
Özgür Hasan Aytar <zrhaytar@gmail.com>
2018-07-16 22:27:55 +0300

    second commit

---
+++ MvcLoginApp/Controllers/IptalController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcLoginApps.Controllers
+{
+    public class IptalController : Controller
+    {
+        //
+        // GET: /Iptal/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
